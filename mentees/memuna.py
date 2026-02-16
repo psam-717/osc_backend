dec68d0345090cbdfa391e4d373d573d39915413
psam-717 <155116265+psam-717@users.noreply.github.com>
2026-02-16 19:57:38 +0000

    memuna's submission

---
+++ mentees/memuna.py
@@ -0,0 +1,48 @@
+products= {
+    "Ankara Fabric" : 120.50,
+    "Plantain chips" : 15.00,
+    "Palm oil": 45.00,
+    "Waakye": 25,
+    "Fufu portion" : 35.00
+}
+
+carts= {}
+
+subtotal= 0
+discount = 0
+cart = ["Ankara Fabric","Plantain chips","Palm oil","Waakye","Fufu portion"]
+counts= len(cart)
+
+if cart == []:
+    print("The cart is empty")
+print(f"Your Cart items are:")    
+for itm in cart:
+    
+    if itm in products:
+        subtotal+=products[itm]
+        if itm in carts:
+            carts[itm] += 1
+        
+        else:
+            carts[itm] = 1    
+        print(f"{itm} : {carts}")
+        
+        
+    else:
+        print(f"{itm} does not exist in our store")
+
+print(f"The number of items are {counts}")
+print(f"The subtotal is {subtotal}")
+
+if subtotal>200:
+    print(f"Discount is 10%")
+    discount = 0.01 * subtotal 
+elif subtotal>100 and subtotal<=200:
+    print("Discount is 5%")
+    discount= 0.05 * subtotal
+
+else:
+    print("There is no discount")
+    discount= 0
+
+print(f"The total is {discount}")
